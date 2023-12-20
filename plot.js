e9ba40f72376c1aaaa6eaa13bbe006e783b57506
Josh Errickson <josh@errickson.net>
2023-12-20 14:24:02 -0500

    Plotting working

--- plot.js
+++ plot.js
@@ -1,42 +1,60 @@
 const ctx = document.getElementById("myChart").getContext("2d");
+let myChart
 
-fetch('data/lowcorr.json')
-  .then((response) => response.json())
-  .then((jsonData) => {
+function updatePlot(path) {
+  fetch(path)
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    })
+    .then((jsonData) => {
 
-    var data = jsonData.map(entry => ({
-      x: entry.b_1,
-      y: entry.b_2
-    }));
+      var data = jsonData.map(entry => ({
+        x: entry.b_1,
+        y: entry.b_2
+      }));
 
 
-    const scatterData = {
-      datasets: [{
-        label: 'Scatter Plot',
-        data: data,
-        backgroundColor: 'rgba(75, 192, 192, 0.5)', // Adjust the color as needed
-        pointRadius: 8, // Adjust the point size as needed
-      }]
-    };
+      const scatterData = {
+        datasets: [{
+          label: 'Scatter Plot',
+          data: data,
+          backgroundColor: 'rgba(75, 192, 192, 0.5)', // Adjust the color as needed
+          pointRadius: 8, // Adjust the point size as needed
+        }]
+      };
 
-    const scatterOptions = {
-      devicePixelRatio: 4, // fixes blurry-on-firefox issue
-      responsive: false,
-      scales: {
-        x: {
-          type: 'linear',
-          position: 'bottom'
-        },
-        y: {
-          type: 'linear',
-          position: 'left'
+      const scatterOptions = {
+        devicePixelRatio: 4, // fixes blurry-on-firefox issue
+        responsive: false,
+        events: [],
+        plugins: { legend: { display: false }, },
+        scales: {
+          x: {
+            type: 'linear',
+            position: 'bottom'
+          },
+          y: {
+            type: 'linear',
+            position: 'left'
+          }
         }
-      }
-    };
+      };
 
-    const myChart = new Chart(ctx, {
-      type: "scatter",
-      data: scatterData,
-      options: scatterOptions,
-    });
-  })
+
+      if (myChart) {
+        myChart.destroy();
+      }
+
+      myChart = new Chart(ctx, {
+        type: "scatter",
+        data: scatterData,
+        options: scatterOptions,
+      });
+    })
+}
+
+updatePlot('data/lowcorr.json')
+// updatePlot('data/modcorr.json')
+// updatePlot('data/highcorr.json')
+// updatePlot('data/extremecorr.json')
