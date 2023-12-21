7ac6062352fc01b5ad5d186319ae3f9aef26d0d7
Josh Errickson <josh@errickson.net>
2023-12-20 21:45:19 -0500

    More work on visualiation

--- docs/corrtext.js
+++ docs/corrtext.js
@@ -1,4 +1,5 @@
 const correlation = document.getElementById("x1x2corr");
+const corrtext = document.getElementById("x1x2corr_text");
 
 // Change the text dynamically
 function updateCorrelation(level) {
@@ -16,4 +17,27 @@ function updateCorrelation(level) {
     correlation.innerHTML = "0.995";
     break;
   }
+}
+
+function updateCorrText(level) {
+  switch(level) {
+  case "low":
+    corrtext.innerHTML = "We see no relationship between the estimated coefficients, and each are well centered around the truth.";
+    break;
+  case "mod":
+    corrtext.innerHTML = "While there is some semblance of a relationship, it is not very strong.";
+    break;
+  case "high":
+    corrtext.innerHTML = "There’s a strong relationship now, such that if one coefficient is incorrectly high, the other is incorrectly low.";
+    break;
+  case "extreme":
+    corrtext.innerHTML = "We now see a very strong relationship between the coefficients.";
+    break;
+  }
+}
+
+function updateAllCorrelation(level) {
+  updatePlot(`data/${level}corr.json`)
+  updateCorrelation(level)
+  updateCorrText(level)
 }
