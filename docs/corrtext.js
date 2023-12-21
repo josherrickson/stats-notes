0bed77e4fe8c4ba035008797282c07fd4889c0ca
Josh Errickson <josh@errickson.net>
2023-12-21 06:19:03 -0500

    Switch from manual to listener

--- docs/corrtext.js
+++ docs/corrtext.js
@@ -1,43 +1,46 @@
-const correlation = document.getElementById("x1x2corr");
-const corrtext = document.getElementById("x1x2corr_text");
 
-// Change the text dynamically
-function updateCorrelation(level) {
-  switch(level) {
-  case "low":
-    correlation.innerHTML = "0.002";
-    break;
-  case "mod":
-    correlation.innerHTML = "0.553";
-    break;
-  case "high":
-    correlation.innerHTML = "0.893";
-    break;
-  case "extreme":
-    correlation.innerHTML = "0.995";
-    break;
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // Get radio buttons and text containers
+  const radioButtons = document.
+        querySelectorAll('input[name="collinearity_strength"]');
 
-function updateCorrText(level) {
-  switch(level) {
-  case "low":
-    corrtext.innerHTML = "We see no relationship between the estimated coefficients, and each are well centered around the truth.";
-    break;
-  case "mod":
-    corrtext.innerHTML = "While there is some semblance of a relationship, it is not very strong.";
-    break;
-  case "high":
-    corrtext.innerHTML = "There’s a strong relationship now, such that if one coefficient is incorrectly high, the other is incorrectly low.";
-    break;
-  case "extreme":
-    corrtext.innerHTML = "We now see a very strong relationship between the coefficients.";
-    break;
-  }
-}
+  // Get text to change
+  const textContainers = document.querySelectorAll('.text-container');
+  const correlation = document.getElementById("x1x2corr");
 
-function updateAllCorrelation(level) {
-  updatePlot(`data/${level}corr.json`)
-  updateCorrelation(level)
-  updateCorrText(level)
-}
+
+  // Attach change event listeners to radio buttons
+  radioButtons.forEach(function (radioButton) {
+    radioButton.addEventListener('change', function () {
+      // Get the selected radio button
+      const level = document.
+            querySelector('input[name="collinearity_strength"]:checked').value;
+
+      /**** Changing correlation value ****/
+      switch(level) {
+      case "low":
+        correlation.innerHTML = "0.002";
+        break;
+      case "mod":
+        correlation.innerHTML = "0.553";
+        break;
+      case "high":
+        correlation.innerHTML = "0.893";
+        break;
+      case "extreme":
+        correlation.innerHTML = "0.995";
+        break;
+      }
+
+
+      /**** Changing text paragraph ****/
+      // Hide all text containers
+      textContainers.forEach(function (container) {
+        container.style.display = 'none';
+      });
+
+      // Show the selected text container
+      document.getElementById(level + 'Text').style.display = 'block';
+    });
+  });
+});
