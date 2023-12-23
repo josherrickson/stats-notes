7704c040c95b93e301c892b6ac8b582a5d3a6846
Josh Errickson <josh@errickson.net>
2023-12-23 14:03:56 -0500

    Finished script for index, cleaned up index and makefile

--- Makefile
+++ Makefile
@@ -2,27 +2,26 @@ SRC_DIR = files
 DEST_DIR = docs
 
 HTML_FILES := $(wildcard $(SRC_DIR)/*.html)
-HTML_DEST_FILES := $(patsubst $(SRC_DIR)/$.html, $(DEST_DIR)/$.html, $(HTML_FILES))
+HTML_DEST_FILES := $(patsubst $(SRC_DIR)/%.html, $(DEST_DIR)/%.html, $(HTML_FILES))
 
 .PHONY: all
 all: $(HTML_DEST_FILES) copy_files docs/index.html docs/responsesurfaceplot.html
 
-docs/index.html: index.html header.html footer.html
+docs/index.html: index.html header.html
 	mkdir -p docs
 	cp header.html header-tmp.html
 	sed -i '' -e 's_TITLE_JE Statisics Notes_' header-tmp.html
-	cat header-tmp.html index.html footer.html > $@
+	cat header-tmp.html index.html > $@
 	rm header-tmp.html
 
-$(DEST_DIR)/$.html: $(SRC_DIR)/$.html header.html footer.html
+$(DEST_DIR)/%.html: $(SRC_DIR)/%.html header.html footer.html
 	mkdir -p docs
 	cp header.html header-tmp.html
 	cp $< tmp.html
 	$(eval TITLE := $(shell sed -n '/<h1>\(.*\)<\/h1>/s//\1/p' $< | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$$//' | sed 's/<\/code>//g; s/<code>//g'))
 	sed -i '' -e 's_TITLE_$(TITLE)_' header-tmp.html
 	cat header-tmp.html tmp.html footer.html > $@
-	rm header-tmp.html
-	rm tmp.html
+	rm header-tmp.html tmp.html
 
 docs/responsesurfaceplot.html: files/responsesurfaceplot.Rmd
 	R -e 'getwd(); rmarkdown::render("$<", output_file="../$@")'
@@ -32,9 +31,9 @@ clean:
 	rm -rf $(DEST_DIR)
 
 copy_files:
+	mkdir -p docs/data
 	cp *.js docs/
 	cp style.css docs/
 	rm -rf docs/images
 	cp -r files/images docs/
-	mkdir -p docs/data
 	cp files/visualizeCollinearity-files/*.json docs/data
--- docs/index_filter.js
+++ docs/index_filter.js
@@ -1,59 +1,78 @@
- // Get the input element and the content container
- const filterInput = document.getElementById('filterInput');
+// Get user-input
+const filterInput = document.getElementById('filterInput')
+
+// Get list of all topics
  const subjectList = document.querySelectorAll('.index_li')
 
- // Add an event listener to the input
- filterInput.addEventListener('input', highlightOccurrences);
+// Add an event listener to the input
+filterInput.addEventListener('input', highlightOccurrences)
 
 function highlightOccurrences() {
-   subjectList.forEach((x) => {
-     const toReplace = filterInput.value.trim()
+  subjectList.forEach((x) => {
+    const toReplace = filterInput.value.trim()
 
-     // First remove any existing highlighting
-     const regexRemoveHighlight = /<span class=\"highlight\">([^<]*)<\/span>/g;
-     x.innerHTML = x.innerHTML.replace(regexRemoveHighlight, '$1');
+    // First remove any existing highlighting
+    const regexRemoveHighlight = /<span class=\"highlight\">([^<]*)<\/span>/g
+    x.innerHTML = x.innerHTML.replace(regexRemoveHighlight, '$1')
 
-     // If the user has input anything
-     if (toReplace.length != 0) {
-       const htmlString = x.innerHTML.match(/<[^>]*>|[^<]+/g)
+    // If the user has input anything
+    if (toReplace.length !== 0) {
+      const htmlString = x.innerHTML.match(/<[^>]*>|[^<]+/g)
 
-       const newString = htmlString.map((s) => {
-         // Add span if not currently a tag
-         if (s.charAt(0) != "<") {
-           // 'g' means match all; 'i' means ignore case
-           let regex = new RegExp(`(${toReplace})`, 'gi');
-           return s.replaceAll(regex,
-                               `<span class="highlight">$1</span>`)
-         } else {
-           // Return tags directly.
-           return s
-         }
-       })
+      const newString = htmlString.map((s) => {
+        // Add span if not currently a tag
+        if (s.charAt(0) !== '<' ) {
+          // 'g' means match all; 'i' means ignore case
+          let regex = new RegExp(`(${toReplace})`, 'gi')
+          return s.replaceAll(regex,
+                              `<span class="highlight">$1</span>`)
+        } else {
+          // Return tags directly.
+            return s
+        }
+      })
 
-       x.innerHTML = newString.join('')
-     }
-   })
- }
+      x.innerHTML = newString.join('')
+    }
+  })
+}
 
 
 // Add an event listener to the input
-filterInput.addEventListener('input', filterList);
+filterInput.addEventListener('input', filterList)
 
 function filterList() {
-  // Get the user input and convert it to lowercase for case-insensitive matching
-  const filterValue = filterInput.value.trim();
+  const toReplace = filterInput.value.trim()
+
+  // Short-circuit if no input
+  if (toReplace.length === 0) {
+    subjectList.forEach((sl) => {
+      sl.style.display = ''
+    })
+    return
+  }
 
   subjectList.forEach((sl) => {
-    // Get the text subjectList of the current list item and convert it to lowercase
     const listItemText = sl.textContent
 
-    // Check if the list item contains the user input
-    if (listItemText.includes(filterValue)) {
-      // If it does, display the list item
+    // Check if the list item contains the user input, if not hide it
+    const toReplaceRegex = new RegExp(`${toReplace}`, 'i')
+    if (toReplaceRegex.test(listItemText)) {
+      // Matches, so keep it
       sl.style.display = ''
     } else {
-      // If it doesn't, hide the list item
+      // Doesn't match, hide it.
       sl.style.display = 'none'
     }
   })
 }
+
+const resetButton = document.getElementById('resetButton');
+
+resetButton.addEventListener('click', () => {
+  filterInput.value = ''
+  // After emptying, force highlighting and filter-ing to re-run to unhighlight
+  // and unfilter everything.
+  filterList()
+  highlightOccurrences()
+})
