87fdcde3707c71f9f42c6a14327ef208c39d4cc3
Josh Errickson <josh@errickson.net>
2023-12-25 13:41:54 -0500

    Improvements to Poisson

--- files/poissonapprox_files/poisson_approx.do
+++ files/poissonapprox_files/poisson_approx.do
@@ -1,4 +1,4 @@
-cd "/Users/josh/repositories/_websites/toc_test/files/poissonapprox_files"
+cd "/Users/josh/repositories/_websites/stats-notes/files/poissonapprox_files"
 cap prog drop binsim
 program def binsim, rclass
     drop _all
@@ -26,22 +26,23 @@ program def binsim, rclass
 end
 
 local beta1 .4
-local reps 10
+local reps 1000
 
-foreach level of numlist 10 5 3 1 {
+foreach level of numlist 10 5 3 1 .5 {
 
 	local prev = `level'/100
-simulate pp=r(pp) b_pois=r(b_pois) b_logit=r(b_logit), ///
+	simulate pp=r(pp) b_pois=r(b_pois) b_logit=r(b_logit), ///
     reps(`reps') nodots: binsim 10000 `prev' `beta1'
 	
-twoway kdensity b_logit || kdensity b_pois, ///
+	twoway kdensity b_logit || kdensity b_pois, ///
     xline(`beta1') legend(label(1 "Logistic") label(2 "Poisson")) 
 
-graph export "../images/poisson`level'.svg", replace
+	graph export "../images/poisson`level'.svg", replace as(svg)
 	
-twoway kdensity b_logit || kdensity b_pois, ///
+	twoway (kdensity b_logit, lcolor("red")) ///
+		   (kdensity b_pois, lcolor("green")), ///
     xline(`beta1') legend(label(1 "Logistic") label(2 "Poisson")) ///
 	scheme(black_hue) 
 	
-graph export "../images/poisson`level'_dark.svg", replace
+	graph export "../images/poisson`level'_dark.svg", replace as(svg)
 }
--- poisson_tabs.js
+++ poisson_tabs.js
@@ -30,6 +30,11 @@ function handleTabClick(level) {
       sim_prop_2.innerHTML = '.01'
       sim_prop_3.innerHTML = '.01'
       break;
+    case '.5':
+      sim_prop_1.innerHTML = '.005'
+      sim_prop_2.innerHTML = '.005'
+      sim_prop_3.innerHTML = '.005'
+      break;
   }
 
   /**** Changing code and plots ****/
@@ -77,11 +82,17 @@ document.getElementById('tab1perc').
            handleTabClick('1')
          })
 
+document.getElementById('tab.5perc').
+         addEventListener('click', function () {
+           handleTabClick('.5')
+         })
+
 // dark-mode/light-mode images
 const image10 = document.getElementById('p-image-10')
 const image5 = document.getElementById('p-image-5')
 const image3 = document.getElementById('p-image-3')
 const image1 = document.getElementById('p-image-1')
+const image_5 = document.getElementById('p-image-.5')
 
 const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)')
 
@@ -91,11 +102,13 @@ function updateColorScheme(level) {
     image5.src = 'images/poisson5_dark.svg'
     image3.src = 'images/poisson3_dark.svg'
     image1.src = 'images/poisson1_dark.svg'
-  } else {
+    image_5.src = 'images/poisson.5_dark.svg'
+   } else {
     image10.src = 'images/poisson10.svg'
     image5.src = 'images/poisson5.svg'
     image3.src = 'images/poisson3.svg'
     image1.src = 'images/poisson1.svg'
+    image_5.src = 'images/poisson.5.svg'
   }
 }
 
